92bc9f4648b3432f7edfb3948a7bbc0f7d04aa6b
Oscar Vargas <oscar@example.com>
2021-12-01 11:18:57 +0100

    Init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        username: "root",
+        password: "1234",
+        database: "dices",
+        host: "localhost"
+    }
+}
---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./database/db');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.listen(PORT, async function() {
+    console.log(`App runining on http://localhost:${PORT}`);
+
+    // Database connection
+    //! Force: true -> DROP TABLES
+    try {
+        await sequelize.sync({ force: false });
+        console.log('Database connection successful');
+    } catch (error) {
+        console.log('Database connection failed', error);
+    }
+    
+});
