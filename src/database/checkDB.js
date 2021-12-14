3d822453c5eacfdd06199da8e19fd4141bb6639c
Oscar <oscar.andres.vargas1@gmail.com>
2021-12-14 19:39:39 +0100

    dbCheck

---
+++ src/database/checkDB.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+const { database } = require("../../config");
+
+// Create Database if does not exists
+mysql.createConnection({
+    user     : database.username,
+    password : database.password
+}).then((connection) => {
+    connection.query('CREATE DATABASE IF NOT EXISTS dices;').then(() => {
+        console.log("DATABASE 'dices' checked");
+        process.exit(); // Ctrl + C command line but in script
+    })
+});
+
+
+// ? Drop 'dices' Database & create a new one
+// mysql.createConnection({
+//     user     : database.username,
+//     password : database.password
+// }).then((connection) => {
+//     connection.query('DROP DATABASE IF EXISTS dices;')
+//         .then(() => connection.query('CREATE DATABASE dices')
+//         .then(() => {
+//             console.log("Database 'dices' created");
+//             process.exit(); // Ctrl + C command line but in script
+//         }))
+// });
